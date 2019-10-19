0a8cf46368d50a77586ae88087d1add06a5b581d
allison cortez <allizetorc@gmail.com>
2019-10-19 16:48:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,10 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash[:winter].map do |holiday, supply|
-    if holiday 
-      holiday << supply
-end
+  holiday_hash[:winter].map do |key, value|
+    value
+ end.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
