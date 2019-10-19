674a0ed44f05ec1930d151b701131438dc2e946b
allison cortez <allizetorc@gmail.com>
2019-10-19 16:40:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,6 @@ def all_supplies_in_holidays(holiday_hash)
         holiday_hash.season.values.flatten
 end
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
