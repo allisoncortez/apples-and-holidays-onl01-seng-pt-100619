1db96d1fbbee42268cf908b5ee60a338f44076c5
allison cortez <allizetorc@gmail.com>
2019-10-19 17:09:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supplies|
+      puts "#{event.to_s.split("_").map { |x| x.capitalize}.join(" ")}"
   
 end
 
