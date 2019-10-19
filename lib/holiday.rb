f14bebfc1e81d5cded64b49e37ec6a224b57d210
allison cortez <allizetorc@gmail.com>
2019-10-19 16:15:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_supplies[:spring][:memorial_day][1] = "Table Cloth"
+  holiday_supplies[:spring][:memorial_day] << "Table Cloth"
   
   # remember to return the updated hash
   holiday_supplies
